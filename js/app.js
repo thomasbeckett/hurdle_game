d6e707e9137eba071aabed5aeb35d8cff648c09e
tech-a49 <tech-a49@Tech-A49s-MacBook-Air.local>
2018-09-17 10:20:02 +0100

    added a jumping mechanism

---
+++ js/app.js
@@ -0,0 +1,118 @@
+$(document).ready(function(){
+
+  var interval;
+  var interval2;
+
+  //character
+  var character = $("#character");
+
+  var characterRight;
+  var characterLeft;
+  var characterTop;
+  var characterBott;
+
+  //board
+  var board = $("#container");
+
+  var boardLeft;
+  var boardRight;
+  var boardTop;
+  var boardBott;
+
+  var yacceleration = 0.05;
+  var yvelocity = -5;
+
+  var ypos = 250;
+
+  var jumping = false;
+   // $("#btn").click(game);
+   //while(jumping == false){
+   $("body").keydown(function (e) {
+     if(e.keyCode == 32){
+       game();
+       jumping = true;
+     }
+
+   });
+ //};
+
+  //game();
+  function game(){
+
+    interval = setInterval(function(){
+      //get positions
+      characterPosition();
+      boardPosition();
+
+
+      //on spacebar move the character
+
+      character.css({
+        "top": ypos + "px"
+      });
+      move();
+      verticalCollisions();
+      // $("body").keydown(function (e) {
+      //   if(e.keyCode == 32){
+      //     jump();
+      //     jumping = true;
+      //   }
+      //
+      // });
+
+      //move the obstacles
+
+    },5)
+  }
+
+
+  function move(){
+    yvelocity += yacceleration;
+    ypos+=yvelocity;
+  };
+
+
+
+  function characterPosition(){
+    // Find the left and top edge of the ball
+    characterLeft = character.offset().left;
+    characterTop = character.offset().top;
+
+    // Find right and bottom edge of the ball
+    characterRight = characterLeft + character.width();
+    characterBott = characterTop + character.height();
+  };
+
+  function boardPosition(){
+    // Find the left and top edge of the ball
+    boardLeft = board.offset().left;
+    boardTop = board.offset().top;
+
+    // Find right and bottom edge of the ball
+    boardRight = boardLeft + board.width();
+    boardBott = boardTop + board.height();
+  }
+
+
+
+  function verticalCollisions(){
+    if (jumping == true) {
+      console.log(2);
+      jumping = false;
+
+    }else{
+      if (characterBott >= boardBott) {
+        console.log(3);
+      yvelocity = 0;
+      yacceleration = 0;
+      ypos = 280;
+      }
+    }
+  }
+
+
+
+
+
+
+});
